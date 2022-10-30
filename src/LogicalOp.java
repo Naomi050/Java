572bdfaec1369e0815061ed5256d0b6d5d812eed
Naomi <naomi.bala2003@yahoo.com>
2022-10-30 23:27:15 +0200

    tema lab11

--- src/LogicalOp.java
+++ src/LogicalOp.java
@@ -1,102 +1,217 @@
-//ex2
+
 public class LogicalOp {
 
+//
+//    //ex1
+//    public void PrintNumbers(int number) {
+//        for (int w = number; w <= 100; w++) {
+//            System.out.println(w);
+//        }
+//
+//
+//    }
+//
+//    //ex2
+//    public void PrintNumbers2(int print) {
+//        for (int y = print; y >= -100; y--) {
+//            System.out.println(y);
+//
+//        }
+//    }
+//
+//
+//    //ex3
+//    public void PrintNumbers3(int first, int second) {
+//
+//        {
+//            for (int x = first; x <= second; x++) {
+//                System.out.println(x);
+//            }
+//        }
+//
+//    }
+//
+//    //ex4
+//    public void PrintNumbers4(int first, int second) {
+//
+//        {
+//            for (int x = first; x >= second; x--) {
+//                System.out.println(x);
+//
+//
+//            }
+//
+//        }
+//    }
+//
+//    //ex5
+//    public void PrintNumbers5(int even) {
+//        for (int l = even; l <= 100; l++) {
+//            if (l % 2 == 0) {
+//                System.out.println(l);
+//            }
+//        }
+//    }
+//
+//    //ex6
+//    public void PrintNumbers6(int odd) {
+//        for (int m = odd; m <= 100; m++) {
+//            if (m % 2 == 1) {
+//                System.out.println(m);
+//            }
+//        }
+//
+//    }
+//
+//    //ex7
+//    int sum = 0;
+//
+//    public void PrintNumbers7(int first, int second) {
+//        for (int i = first; i <= second; i++) {
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+//    }
+//
+//    //ex8
+//    public float PrintNumbers8(int first, int second) {
+//
+//        for (int i = first; i <= second; i++) {
+//            sum = sum + i;
+//
+//        }
+//            return sum/100;
+//
+//        }
+//        //ex9
+//        public String Print( String asterisk) {
+//            for (int y = 0; y <= 6; y++) {
+//                for (int x = 6; x >= y; x--) {
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+//            return asterisk;
+//        }
 
+    //Java While Loop
     //ex1
-    public void PrintNumbers(int number) {
-        for (int w = number; w <= 100; w++) {
-            System.out.println(w);
+    public void While(int first) {
+        while (first <= 100) {
+            System.out.println(first);
+            first++;
         }
-
-
     }
 
     //ex2
-    public void PrintNumbers2(int print) {
-        for (int y = print; y >= -100; y--) {
-            System.out.println(y);
-
+    public void While2(int number) {
+        while (number >= -100) {
+            System.out.println(number);
+            number--;
         }
     }
-
 
     //ex3
-    public void PrintNumbers3(int first, int second) {
-
-        {
-            for (int x = first; x <= second; x++) {
-                System.out.println(x);
-            }
+    public void While3(int first, int second) {
+        while (first <= second) {
+            System.out.println(first);
+            first++;
         }
-
     }
 
+    int x = 183;
+    int y = 114;
+
     //ex4
-    public void PrintNumbers4(int first, int second) {
-
-        {
-            for (int x = first; x >= second; x--) {
-                System.out.println(x);
-
+    public void While4(int first, int second) {
+        if (x < y) {
+            while (first <= second) {
+                System.out.println(first);
+                first++;
+            }
+        } else if (x > y) {
+            while (first >= second) {
+                System.out.println(second);
+                second++;
 
             }
 
         }
+
     }
 
     //ex5
-    public void PrintNumbers5(int even) {
-        for (int l = even; l <= 100; l++) {
-            if (l % 2 == 0) {
-                System.out.println(l);
-            }
+    int i = 2;
+
+    public int While5(int even) {
+        while (i <= 100) {
+            System.out.println(i + even);
+            i = i + 2;
         }
+
+        return even;
     }
 
     //ex6
-    public void PrintNumbers6(int odd) {
-        for (int m = odd; m <= 100; m++) {
-            if (m % 2 == 1) {
-                System.out.println(m);
-            }
-        }
+    int m = 1;
 
+    public int While6(int odd) {
+        while (m <= 100) {
+            System.out.println(m + odd);
+            m = m + 2;
+        }
+        return odd;
     }
 
     //ex7
-    int sum = 0;
+    public float While7(int first, int second) {
+        float sum = 0;
+        float count = 0;
+        while (first <= second) {
+            sum = sum + first;
+            count++;
+            first++;
 
-    public void PrintNumbers7(int first, int second) {
-        for (int i = first; i <= second; i++) {
-            sum = sum + i;
         }
-        System.out.println(sum);
+        return sum / count;
+
     }
 
     //ex8
-    public float PrintNumbers8(int first, int second) {
+    public int While8(int first, int second) {
+        int sum = 0;
+        int count = 0;
+            while (first <= second) {
+                if (first % 7 == 00)
+                sum = sum + first;
+                count++;
+                first++;
 
-        for (int i = first; i <= second; i++) {
-            sum = sum + i;
 
-        }
-            return sum/100;
-
-        }
-        //ex9
-        public String Print( String asterisk){
-        for(int y = 0; y <=6; y++){
-            for ( int x = 6; x>=y; x--){
-                System.out.print("*");
             }
-                System.out.println();
-            }
-            return asterisk;
-        }
+
+        return sum/count;
+    }
+
+
+    //ex9
 
 
 
-        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,46 @@ public class Main {
 
     public static void main(String[] args) {
     LogicalOp op = new LogicalOp();
-    int w = 30;
-    op.PrintNumbers(w);
-    int y = -40;
-    op.PrintNumbers2(y);
-    int x = 25;
-    int z = 75;
-    op.PrintNumbers3(x,z);
-    int i = 98;
-    int l = 50;
-    op.PrintNumbers4(i,l);
-    int a = 1;
-    op.PrintNumbers5(a);
-    int b = 2;
-    op.PrintNumbers6(b);
-    int d = 45;
-    int c = 100;
-    op.PrintNumbers7(d,c);
-    int e =60;
-    int t = 100;
-    op.PrintNumbers8(e,t);
-    String v = "asterisk";
-    op.Print(v);
-
+//    int w = 30;
+//    op.PrintNumbers(w);
+//    int y = -40;
+//    op.PrintNumbers2(y);
+//    int x = 25;
+//    int z = 75;
+//    op.PrintNumbers3(x,z);
+//    int i = 98;
+//    int l = 50;
+//    op.PrintNumbers4(i,l);
+//    int a = 1;
+//    op.PrintNumbers5(a);
+//    int b = 2;
+//    op.PrintNumbers6(b);
+//    int d = 45;
+//    int c = 100;
+//    op.PrintNumbers7(d,c);
+//    int e =60;
+//    int t = 100;
+//    op.PrintNumbers8(e,t);
+//    String v = "asterisk";
+//    op.Print(v);
+        int z = 17;
+        op.While(z);
+        int a = -28;
+        op.While2(a);
+        int x = 175;
+        int y = 230;
+        op.While3(x,y);
+        int f= 183;
+        int e= 114;
+        op.While4(f, e);
+        int b = 1;
+        op.While5(b);
+        int m = 1;
+        op.While6(m);
+        System.out.println(op.While7(111,88999));
+        int o= 1;
+        int p = 50;
+        op.While8(o, p);
 
 
     }
