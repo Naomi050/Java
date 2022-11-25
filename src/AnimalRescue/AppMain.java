5d4f6d544a36b1f2883b98f3e53f33e684d7f6c2
Naomi <naomi.bala2003@yahoo.com>
2022-11-25 17:09:14 +0200

    tema lab 13

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -4,13 +4,13 @@ public class AppMain {
 
  public static void main(String[] args) {
   Cat fofo = new Cat();
-  fofo.setName("Fofo");
+  fofo.setName(" NAME: Fofo");
   System.out.println(fofo.getName());
   fofo.setAge(3);
   System.out.println(fofo.getAge());
-  fofo.setColor("White");
+  fofo.setColor(" COLOR: White");
   System.out.println(fofo.getColor());
-  fofo.setGender("female");
+  fofo.setGender(" GENDER: female");
   System.out.println(fofo.getGender());
   fofo.setWeight(2);
   System.out.println(fofo.getWeight());
@@ -18,11 +18,11 @@ public class AppMain {
   System.out.println(fofo.getHungerlevel());
   fofo.setMoodlevel(90);
   System.out.println(fofo.getMoodlevel());
-  fofo.setThefoodname("milk with milk");
+  fofo.setThefoodname(" FOOD: milk with milk");
   System.out.println(fofo.getThefoodname());
   fofo.setThelevelofhealth(95);
   System.out.println(fofo.getThelevelofhealth());
-  fofo.setThenameoftherecreationalactivity("to play in the water");
+  fofo.setThenameoftherecreationalactivity("FAVORITE GAME: to play in the water");
   System.out.println(fofo.getThenameoftherecreationalactivity());
 
 
@@ -52,5 +52,72 @@ public class AppMain {
   System.out.println(adopter.getName());
   adopter.setTheamountofmoneyavailable("3000$");
   System.out.println(adopter.getTheamountofmoneyavailable());
+  System.out.println();
+  System.out.println();
+
+
+
+
+
+  Dog dog = new Dog();
+  dog.setName("The dog's name is Rex");
+  System.out.println(dog.getName());
+  dog.setAge(5);
+  System.out.println(dog.getAge());
+  dog.setColor("The dog's color is brown");
+  System.out.println(dog.getColor());
+  dog.setGender("The dog's gender is male");
+  System.out.println(dog.getGender());
+  dog.setWeight(6);
+  System.out.println(dog.getWeight());
+  System.out.println();
+  System.out.println();
+
+
+  Hen hen = new Hen();
+  hen.setName("The hen's name is Charlie ");
+  System.out.println(hen.getName());
+  hen.setColor("The hen's color is orange");
+  System.out.println(hen.getColor());
+  hen.setAge(2);
+  System.out.println(hen.getAge());
+  hen.setGender("The hen's gender is female");
+  System.out.println(hen.getGender());
+  hen.setWeight(1);
+  System.out.println(hen.getWeight());
+  System.out.println();
+  System.out.println();
+
+
+  Lamb lamb = new Lamb();
+  lamb.setName("The lamb's name is Molly ");
+  System.out.println(lamb.getName());
+  lamb.setColor("The lamb's clor is white");
+  System.out.println(lamb.getColor());
+  lamb.setGender("The lamb's gender is female");
+  System.out.println(lamb.getGender());
+  lamb.setAge(3);
+  System.out.println(lamb.getAge());
+  lamb.setWeight(2);
+  System.out.println(lamb.getWeight());
+  System.out.println();
+  System.out.println();
+
+
+  Rabit rabit = new Rabit();
+  rabit.setName("The rabit's name is Dotty");
+  System.out.println(rabit.getName());
+  rabit.setColor("The rabit's color is gray");
+  System.out.println(rabit.getColor());
+  rabit.setGender("The rabit's gender is male");
+  System.out.println(rabit.getGender());
+  rabit.setAge(4);
+  System.out.println(rabit.getAge());
+  rabit.setWeight(5);
+  System.out.println(rabit.getWeight());
+
  }
+
+
+
 }
